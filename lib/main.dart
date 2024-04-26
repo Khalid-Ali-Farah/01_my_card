6d61c3a12363871a1db16f86ccfd63599f6febd4
khalid ali <khalid.ali.farah.15@gmail.com>
2024-04-26 10:18:15 +0800

    I have made some improvement into main file.

--- lib/main.dart
+++ lib/main.dart
@@ -7,129 +7,142 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: Color(0XFFD9D9D9),
-        body: Column(
+        backgroundColor: Colors.grey,
+        body: SafeArea(
+            child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Column(
-              children: [
-                CircleAvatar(
-                  backgroundImage: AssetImage(
-                    'images/user.png',
-                  ),
-                  // backgroundColor: Colors.black,
-
-                  radius: 65.0,
-                ),
-                Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: Text(
-                    'Khalid Ali',
-                    style: TextStyle(
-                      fontSize: 30.0,
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontFamily: 'RobotoSlab',
-                    ),
-                  ),
-                ),
-                Text(
-                  'SOFTWARE DEVELOPER',
-                  style: TextStyle(
-                    fontSize: 22.0,
-                    color: Colors.black,
-                  ),
-                ),
-                Text(
-                  textAlign: TextAlign.center,
-                  'Software Developer Front-End Development Mobile App Development Enthusiast',
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Color(0xFF726969),
-                  ),
-                ),
-              ],
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/user.png'),
+            ),
+            Text(
+              'KHALID ALI',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'SOFTWARE DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Roboto',
+                color: Colors.white,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.white,
+              ),
             ),
             Padding(
-              padding: EdgeInsets.symmetric(horizontal: 77.0, vertical: 20.0,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+              padding: const EdgeInsets.symmetric(vertical: 20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                 children: [
                   Row(
                     children: [
-                      Text(
-                        // textAlign: TextAlign.end,
-
-                        'CONTACT',
-                        style: TextStyle(
-                            fontSize: 15.0,
-                            color: Colors.black,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ],
-                  ),
-                  Divider(
-                    height: 10.0,
-                    color: Colors.black,
-                  ),
-                  SizedBox(
-                    height: 15.0,
-                  ),
-                  Row(
-                    children: [
                       Image(
-                        image: AssetImage('images/google.png'),
-                        width: 25.0,
-                        color: Colors.black,
-                      ),
-                      SizedBox(
-                        height: 40.0,
-                        width: 10.0,
-                      ),
-                      Text('khalid.ali.farah.15@gmail.com'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Image(
-                        image: AssetImage('images/git.png'),
-                        width: 25.0,
-                        color: Colors.black,
-                      ),
-                      SizedBox(
-                        height: 40.0,
-                        width: 10.0,
-                      ),
-                      Text('Khalid-Ali-Farah'),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Image(
-                        image: AssetImage('images/in.png'),
-                        width: 25.0,
-                        color: Colors.black,
-                      ),
-                      SizedBox(
-                        height: 40.0,
-                        width: 10.0,
-                      ),
-                      Text(
-                        'khalid-ali-farah',
-                        style: TextStyle(
-                          fontSize: 14.0,
+                        image: AssetImage(
+                          'images/google.png',
                         ),
+                        width: 30.0,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Image(
+                        image: AssetImage(
+                          'images/f.png',
+                        ),
+                        width: 30.0,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Image(
+                        image: AssetImage(
+                          'images/git.png',
+                        ),
+                        width: 30.0,
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      Image(
+                        image: AssetImage(
+                          'images/in.png',
+                        ),
+                        width: 30.0,
                       ),
                     ],
                   ),
                 ],
               ),
             ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.grey[800],
+                  ),
+                  title: Text(
+                    '+60 123456790',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontFamily: 'Roboto',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Colors.grey[800],
+                ),
+                title: Text(
+                  'khalid@email.com',
+                  style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.grey[800],
+                      fontFamily: 'Roboto'),
+                ),
+              ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.language,
+                    color: Colors.grey[800],
+                  ),
+                  title: Text(
+                    'www.khalid.com',
+                    style: TextStyle(
+                      color: Colors.grey[800],
+                      fontFamily: 'Roboto',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
           ],
-        ),
+        )),
       ),
     );
   }
